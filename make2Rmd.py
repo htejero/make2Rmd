a9c03b5fad3e43e4be6a884009c51c8f3196985e
hector.tejero.81@gmail.com <htejero@anakin.cnio.es>
2014-03-04 15:29:26 +0100

    First commented 0.1. version, aka "The linear parser"

--- make2Rmd.py
+++ make2Rmd.py
@@ -1,31 +1,39 @@
-filename = "../Garnett/exome/Makefile"
+# make2Rmd 0.1
+# ==============
 
-input_file = open(filename, "r")
+# make2Rmd pretends to be a tool to transform a makefile into a (Rstudio) Markdown literate programming script. 
+# Version 0.1 is just a parser that linearly wraps the makefile into a Rstudio file. 
+
+filename = "../Garnett/exome/Makefile" #the test file 
+
+input_file = open(filename, "r")    
 output_file =  open(filename + "_wrapped.Rmd", "w")
 
-comment = ""
+
+#The expected structure of a makefile is that of comment->rule->code. In version 0.1 the dependency
+#graph of the makefile is not used. The script just parses the file lienarly detecting comments and including the rule in the code. Each time it detects a change from comment to code, write the comment an then initializes it. Each time it detects a change from code to comment, writes the code and initialized it. 
+
+comment = "" 
 code = ""
 for linea in input_file:
-	linea = linea.lstrip("\n")
+	linea = linea.lstrip("\n") # remove blank lines 
 	if len(linea)>0:
-		if linea[0]=="#": # Es un comentario de Make			
-			linea = linea.lstrip("#")
-			linea = linea.lstrip(" ")
-			comment += linea
-			if code!="":
-				code +="```\n"
-				output_file.write(code)
-				code = ""
-		else:
-			if comment!="":
-				output_file.write(comment + "\n")
-				comment = ""
-			if code=="":
-				code = "```{r, engine='bash', eval=FALSE}\n"
-				#linea = linea.lstrip("\n")
+		if linea[0]=="#": # The line is a comment			
+			linea = linea.lstrip("#")   # Remove the comment mark
+			linea = linea.lstrip(" ")   # Remove the spaces 
+			comment += linea	    
+			if code!="":		    #If code has been detected previuously
+				code +="```\n"	    #Close the chunk 
+				output_file.write(code)  #write in the file 
+				code = ""		 #Initializes the code 
+		else:		 # The line is code 
+			if comment!="":  	#If there a previous comment 
+				output_file.write(comment + "\n")  #write the comment
+				comment = "" 			# initializes it 
+			if code=="":		#if it is the first line of code
+				code = "```{r, engine='bash', eval=FALSE}\n" # open the chunk
 				code += linea
 			else:
-				#linea = linea.lstrip("\n")
 	 			code += linea
 	
 
