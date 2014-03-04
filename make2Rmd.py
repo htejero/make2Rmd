aeb89b5cc9c0f5c88baf04ef62c3cdf545d57b08
hector.tejero.81@gmail.com <htejero@anakin.cnio.es>
2014-03-04 17:00:31 +0100

    Version 0.2. aka "The improved linear parser"
    Parses the dependency graph. Topological sort not implemented.

--- make2Rmd.py
+++ make2Rmd.py
@@ -1,20 +1,38 @@
-# make2Rmd 0.1
+# make2Rmd 0.2
 # ==============
 
 # make2Rmd pretends to be a tool to transform a makefile into a (Rstudio) Markdown literate programming script. 
 # Version 0.1 is just a parser that linearly wraps the makefile into a Rstudio file. 
+# Version 0.2 order the makefile according to the dependency graph 
 
-filename = "../Garnett/exome/Makefile" #the test file 
+filename = "../Signatures/Garnett/exome/Makefile" #the test file 
 
-input_file = open(filename, "r")    
-output_file =  open(filename + "_wrapped.Rmd", "w")
+#OPTIONS 
 
+LINEAR = True 
+    
 
-#The expected structure of a makefile is that of comment->rule->code. In version 0.1 the dependency
-#graph of the makefile is not used. The script just parses the file lienarly detecting comments and including the rule in the code. Each time it detects a change from comment to code, write the comment an then initializes it. Each time it detects a change from code to comment, writes the code and initialized it. 
+import re
+
+#The expected structure of a makefile is that of comment->rule->code. 
+
+# 1. Parse the Makefile to obtain the dependency graph of the rules and the comment_code graph associated. 
+
+input_file = open(filename, "r")
 
 comment = "" 
 code = ""
+
+dependency_graph = dict()
+comment_code_graph = dict()
+makefile_rule_order = list()  # the order in which the rules appear in the file 
+
+#Regex 
+#unixFile = "[\w.]+"  #simple version. Improve!
+rule = re.compile('^([\w.]+)\s*\:\s*(.*)')
+
+
+
 for linea in input_file:
 	linea = linea.lstrip("\n") # remove blank lines 
 	if len(linea)>0:
@@ -24,19 +42,76 @@ for linea in input_file:
 			comment += linea	    
 			if code!="":		    #If code has been detected previuously
 				code +="```\n"	    #Close the chunk 
-				output_file.write(code)  #write in the file 
+				#output_file.write(code)  #write in the file 
+				comment_code_graph[target].append(code)
 				code = ""		 #Initializes the code 
-		else:		 # The line is code 
-			if comment!="":  	#If there a previous comment 
-				output_file.write(comment + "\n")  #write the comment
-				comment = "" 			# initializes it 
-			if code=="":		#if it is the first line of code
-				code = "```{r, engine='bash', eval=FALSE}\n" # open the chunk
-				code += linea
-			else:
-	 			code += linea
-	
 
+		else:		 # The line is code (= not comment)		
+			isRule = re.search(rule, linea)
+			# is it a rule?
+			if isRule: 
+				target = isRule.group(1)
+				dependencies = isRule.group(2).split()
+				dependency_graph[target] = dependencies
+				makefile_rule_order.append(target)		
+				
+			else: 
+				if code=="":		#if it is the first line of code
+					code = "```{r, engine='bash', eval=FALSE}\n" # open the chunk
+					code += linea
+				else:
+	 				code += linea
+
+			if comment!="":  	#If there a previous comment 
+				#output_file.write(comment + "\n")  #write the comment
+				comment_code_graph[target] = [comment]
+				comment = "" 			# initializes it 
+
+
+comment_code_graph[target] = [comment]
+comment_code_graph[target].append(code)
 
 input_file.close()
+
+#print comment_code_graph["DivideByCellLine"][1]
+
+## 2. Sort the graph topologically 
+
+if not LINEAR: 
+	pass
+
+
+# 3. Write the output file according to the graph 
+
+# 3.1. The linear parser 
+output_file =  open(filename + "_wrapped.Rmd", "w")
+
+if LINEAR:
+	#nrule = 1
+	for target in makefile_rule_order:
+		#print nrule	
+		output_file.write(comment_code_graph[target][0])
+		output_file.write("\n")
+		if len(comment_code_graph[target])>1:
+			output_file.write(comment_code_graph[target][1])
+			output_file.write("\n") 
+		#nrule +=1
+
+#3.2. Topologically sorted 
+
+else: 
+
+	targets = dependency_graph.keys()
+
+	Values = dependency_graph.values()
+	files = [x for sublist in Values for x in sublist]
+	files = list(set(files))
+
+
+
+
+
 output_file.close()
+
+
+
